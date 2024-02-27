349474fe37b155794f7409ecca03f8a89d9f6f32
Felicia Jones <feliciaj628@gmail.com>
2024-02-26 23:33:01 -0800

    description box fixes

--- src/components/SameDayReadings.js
+++ src/components/SameDayReadings.js
@@ -15,7 +15,7 @@ function SameDayReadings() {
             backgroundImage="/images/sameday1.png"
             link="https://www.etsy.com/listing/1452630681/how-do-they-really-feel-same-day-psychic?click_key=9deca9b564183d514c0cb7cb9d3c07fb109fb536%3A1452630681&click_sum=4ca87dda&ref=shop_home_active_1&crt=1&sts=1"
           />
-          <div className="description-box">
+          <div className="sd-description-box">
             <p className="title">How do they really feel?</p>
             <hr className="hr-3"></hr>
             <p className="description">
@@ -29,7 +29,7 @@ function SameDayReadings() {
             backgroundImage="/images/sameday2.png"
             link="https://www.etsy.com/listing/1438418684/no-contact-same-day-psychic-tarot?click_key=1f3440b7fed68d0402f4b2598d94b7187c6051de%3A1438418684&click_sum=e12c04ab&ref=shop_home_recs_3&sts=1"
           />
-          <div className="description-box">
+          <div className="sd-description-box">
             <p className="title">Do they still think about you?</p>
             <hr className="hr-3"></hr>
             <p className="description">
@@ -44,7 +44,7 @@ function SameDayReadings() {
             backgroundImage="/images/sameday3.png"
             link="https://www.etsy.com/listing/1452623439/who-is-thinking-about-you-and-why-same?click_key=25502a93cf12225d78aa7ebf7f13af61e66bfabc%3A1452623439&click_sum=18534796&ref=shop_home_recs_7&crt=1&sts=1"
           />
-          <div className="description-box">
+          <div className="sd-description-box">
             <p className="title">Who is thinking about you?</p>
             <hr className="hr-3"></hr>
             <p className="description">
@@ -59,7 +59,7 @@ function SameDayReadings() {
             link="https://www.etsy.com/listing/1287721504/same-day-psychic-tarot-reading-1?click_key=60a189a635de2b4a8c319699b3ccb2a4d2a7994f%3A1287721504&click_sum=18d98e27&ref=shop_home_recs_2&crt=1&sts=1"
           />
 
-          <div className="description-box">
+          <div className="sd-description-box">
             <p className="title">One Question Reading</p>
             <hr className="hr-3"></hr>
             <p className="description">
--- src/pages/OracleDecks.js
+++ src/pages/OracleDecks.js
@@ -32,7 +32,7 @@ function OracleDecks() {
                   backgroundImage="/images/enchanted-meadow.png"
                   link="https://www.etsy.com/listing/1610058297/enchanted-meadow-printable-mini-oracle?click_key=c86cf9a1f299622a0db149c8c8928c582b1002a6%3A1610058297&click_sum=8dd68310&ref=shop_home_active_12&sts=1"
                 />
-                <div className="description-box">
+                <div className="oc-description-box">
                   <p className="title">Enchanted Meadow Oracle</p>
                   {/* <hr className="hr-3"></hr>
                   <p className="description">Printable DIY mini oracle deck</p> */}
@@ -43,7 +43,7 @@ function OracleDecks() {
                   backgroundImage="/images/dear-diary.png"
                   link="https://www.etsy.com/listing/1609537685/dear-diary-printable-mini-oracle-deck-no?click_key=e2af129f350bb37b6a99daa2bc72dd13a5fdf133%3A1609537685&click_sum=740df370&ref=shop_home_active_8&sts=1"
                 />
-                <div className="description-box">
+                <div className="oc-description-box">
                   <p className="title">Dear Diary Oracle - No Contact</p>
                   {/* <hr className="hr-3"></hr>
                   <p className="description">Printable DIY mini oracle deck</p> */}
@@ -54,7 +54,7 @@ function OracleDecks() {
                   backgroundImage="/images/true-desires.png"
                   link="https://www.etsy.com/listing/1595398234/true-desires-printable-mini-oracle-deck?click_key=984eeea339332b48cae122983f761a41441bcdb2%3A1595398234&click_sum=894e0355&ref=shop_home_active_11&sts=1"
                 />
-                <div className="description-box">
+                <div className="oc-description-box">
                   <p className="title">True Desires Oracle</p>
                   {/* <hr className="hr-3"></hr>
                   <p className="description">Printable DIY mini oracle deck</p> */}
